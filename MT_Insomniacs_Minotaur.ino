b0e49921c5054f1e5e335915295d0c5da6065975
Joseph Painter <129447076+JRPainter@users.noreply.github.com>
2023-04-04 22:08:40 -0500

    Working demo
    Changed pin numbers and fixed code so that it worked on demo Arduino

--- MT_Insomniacs_Minotaur.ino
+++ MT_Insomniacs_Minotaur.ino
@@ -1,18 +1,21 @@
 #include <Servo.h>
 
 Servo myservo;
-const int buttonPin = 2;
+const int buttonPin = 9;
 
 void setup() {
-  myservo.attach(9);
+  myservo.attach(11);
   pinMode(buttonPin, INPUT);
   myservo.write(0);
+  Serial.begin(9600);
 }
 
 void loop() {
+  
+  Serial.println(digitalRead(buttonPin));
   if(digitalRead(buttonPin) == HIGH) {
     myservo.write(180);
   } else {
-    myservo.write(0)
+    myservo.write(0);
   }
 }
