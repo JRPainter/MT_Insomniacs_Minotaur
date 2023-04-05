d3511bed333a91c836d32c0dcb47e86c04a72f20
Joseph Painter <129447076+JRPainter@users.noreply.github.com>
2023-04-05 17:01:24 -0500

    Second servo and ultrasound sensor
    Added code for a second sensor and connected it so that it activates whenever an ultrasound sensor senses an object within 7 inches

--- MT_Insomniacs_Minotaur.ino
+++ MT_Insomniacs_Minotaur.ino
@@ -1,21 +1,47 @@
 #include <Servo.h>
 
 Servo myservo;
+Servo myservo2;
 const int buttonPin = 9;
+const int trigPin = 5;
+const int echoPin = 6;
+
+long duration;
+int distance;
 
 void setup() {
   myservo.attach(11);
+  myservo2.attach(13);
   pinMode(buttonPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  myservo.write(0);
   myservo.write(0);
   Serial.begin(9600);
 }
 
 void loop() {
-  
-  Serial.println(digitalRead(buttonPin));
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2;
+
+  //Serial.print("Distance: ");
+  //Serial.println(distance);
+  //Serial.println(digitalRead(buttonPin));
+
   if(digitalRead(buttonPin) == HIGH) {
     myservo.write(180);
   } else {
     myservo.write(0);
+  }
+
+  if(distance < 7) {
+    myservo2.write(180);
+  } else {
+    myservo2.write(0);
   }
 }
