d06e16187c79dbf6e3b6956be8621aa0397abbc8
Joseph Painter <129447076+JRPainter@users.noreply.github.com>
2023-04-11 16:11:27 -0500

    2nd Servo as Button
    Got rid of sensor code and replaced the code with another button

--- MT_Insomniacs_Minotaur.ino
+++ MT_Insomniacs_Minotaur.ino
@@ -1,21 +1,35 @@
 #include <Servo.h>
 
 Servo myservo;
+Servo myservo2;
 const int buttonPin = 9;
+const int buttonPin2 = 7;
+
+
 
 void setup() {
   myservo.attach(11);
+  myservo2.attach(13);
   pinMode(buttonPin, INPUT);
+  pinMode(buttonPin2, INPUT);
   myservo.write(0);
+  myservo2.write(0);
   Serial.begin(9600);
 }
 
 void loop() {
   
-  Serial.println(digitalRead(buttonPin));
+  //Serial.println(digitalRead(buttonPin));
+
   if(digitalRead(buttonPin) == HIGH) {
     myservo.write(180);
   } else {
     myservo.write(0);
+  }
+
+  if(digitalRead(buttonPin2) == HIGH) {
+    myservo2.write(90);
+  } else {
+    myservo2.write(0);
   }
 }
