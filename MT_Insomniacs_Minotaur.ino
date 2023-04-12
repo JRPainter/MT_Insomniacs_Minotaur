6b1b40e37c392c37c50ba98c3657a407e52666ba
Joseph Painter <129447076+JRPainter@users.noreply.github.com>
2023-04-12 18:57:52 -0500

    Press once buttons
    Changed the code so that the buttons only needs to be pressed once rather than held down for the servos to work.

--- MT_Insomniacs_Minotaur.ino
+++ MT_Insomniacs_Minotaur.ino
@@ -4,6 +4,10 @@ Servo myservo;
 Servo myservo2;
 const int buttonPin = 9;
 const int buttonPin2 = 7;
+int buttonState = 0;
+int previousButtonState = 0;
+int button2State = 0;
+int previousButton2State = 0;
 
 
 
@@ -18,18 +22,22 @@ void setup() {
 }
 
 void loop() {
+  buttonState = digitalRead(buttonPin);
+  button2State = digitalRead(buttonPin2);
   
   //Serial.println(digitalRead(buttonPin));
 
-  if(digitalRead(buttonPin) == HIGH) {
+  if((buttonState == HIGH) && (buttonState != previousButtonState)) {
     myservo.write(180);
   } else {
     myservo.write(0);
   }
 
-  if(digitalRead(buttonPin2) == HIGH) {
+  if((button2State == HIGH) && (button2State != previousButton2State)) {
     myservo2.write(90);
   } else {
     myservo2.write(0);
   }
+  previousButtonState = buttonState;
+  previousButton2State = button2State;
 }
