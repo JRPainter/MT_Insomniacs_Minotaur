dd777a0a6f81191765574058454dec0a4dd0815f
Joseph Painter <129447076+JRPainter@users.noreply.github.com>
2023-04-12 23:52:33 -0500

    Hardware Prototype
    Final code made for hardware prototype

--- MT_Insomniacs_Minotaur.ino
+++ MT_Insomniacs_Minotaur.ino
@@ -2,21 +2,23 @@
 
 Servo myservo;
 Servo myservo2;
-const int buttonPin = 9;
-const int buttonPin2 = 7;
+const int buttonPin = 13;
+const int buttonPin2 = 2;
 int buttonState = 0;
 int previousButtonState = 0;
 int button2State = 0;
 int previousButton2State = 0;
+bool buttonPressed = false;
+bool button2Pressed = false;
 
 
 
 void setup() {
-  myservo.attach(11);
-  myservo2.attach(13);
+  myservo.attach(6);
+  myservo2.attach(3);
   pinMode(buttonPin, INPUT);
   pinMode(buttonPin2, INPUT);
-  myservo.write(0);
+  myservo.write(180);
   myservo2.write(0);
   Serial.begin(9600);
 }
@@ -25,19 +27,27 @@ void loop() {
   buttonState = digitalRead(buttonPin);
   button2State = digitalRead(buttonPin2);
   
-  //Serial.println(digitalRead(buttonPin));
 
   if((buttonState == HIGH) && (buttonState != previousButtonState)) {
-    myservo.write(180);
-  } else {
+    buttonPressed = true;
+  }
+
+  if (buttonPressed){
     myservo.write(0);
+  } else {
+    myservo.write(180);
   }
 
   if((button2State == HIGH) && (button2State != previousButton2State)) {
+    button2Pressed = true;
+  } 
+
+  if (button2Pressed){
     myservo2.write(90);
   } else {
     myservo2.write(0);
   }
+
   previousButtonState = buttonState;
   previousButton2State = button2State;
 }
